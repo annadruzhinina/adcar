32fb64efbcbe4dd00a8842d4305bf4b67bd4aa63
anna <anna@rcbd.org>
2020-12-22 23:21:46 -0500

    add project

---
+++ src/About.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+
+import {ABOUT_CONTENT, HERO_CONTENT} from "./constants";
+
+import './styles/typography/text-center.scss'
+import './styles/section.scss'
+import './styles/components/card/card-about.scss'
+import './styles/components/grid.scss'
+
+import SearchIcon from "mdi-react/SearchIcon";
+import DeleteAlertIcon from "mdi-react/DeleteAlertIcon";
+import DeleteAlertOutlineIcon from "mdi-react/DeleteAlertOutlineIcon";
+import DeleteCircleOutlineIcon from "mdi-react/DeleteCircleOutlineIcon";
+import DeleteCircleIcon from "mdi-react/DeleteCircleIcon";
+import FindInPageIcon from "mdi-react/FindInPageIcon";
+import DirectionsCarIcon from "mdi-react/DirectionsCarIcon";
+import AddLocationIcon from "mdi-react/AddLocationIcon";
+
+
+class About extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: ABOUT_CONTENT.list,
+      title: ABOUT_CONTENT.title,
+      description: ABOUT_CONTENT.description
+    }
+  }
+
+  render() {
+    const createDescription = () => {
+      return {__html: this.state.description};
+    }
+    return (
+      <section className="ad-section ad-text-center">
+        <div className="ad-section_container">
+          <h3 className='ad-section_title'>
+            {this.state.title}
+          </h3>
+          <p className="ad-card-about_subtitle"
+             dangerouslySetInnerHTML={createDescription()}>
+          </p>
+          <div className="ad-card-about_grid">
+            {this.state.data.map((card, index) => {
+              return (
+                <div key={index} className="ad-card-about">
+                  <div className="ad-card-about_icon">
+                    {card.icon === 'type1' && <DirectionsCarIcon/>}
+                    {card.icon === 'type2' && <DeleteAlertIcon/>}
+                    {card.icon === 'type3' && <DirectionsCarIcon/>}
+                    {card.icon === 'type4' && <AddLocationIcon/>}
+                    {card.icon === 'type5' && <DeleteCircleIcon/>}
+                    {card.icon === 'type6' && <FindInPageIcon/>}
+                  </div>
+                  <h3 className="ad-card-about_title">{card.title}</h3>
+                  <div>{card.description}</div>
+                </div>
+              )
+            })
+            }
+
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <p>
+      Header here
+    </p>
+  );
+}
+
+export default Header;
